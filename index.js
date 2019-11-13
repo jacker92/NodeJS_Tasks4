665734eea7b132c5a394fc8956728a5548942ede
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2019-11-13 20:59:48 +0200

    Added startup script.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const apiRouter = require('./routes/poisRouter')
+const validator = require('./query_validator')
+
+const app = express();
+const port = 3000;
+
+app.set('json spaces', 2);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}))
+
+app.use('/api/v1', apiRouter);
+
+app.use(validator);
+
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`))
+
+module.exports = app
