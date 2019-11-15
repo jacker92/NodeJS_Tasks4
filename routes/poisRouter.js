c48b98b4079bdc85162f0a2ea57b006648de74a3
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2019-11-15 08:31:20 +0200

    Changed paths in poisRouter.

--- routes/poisRouter.js
+++ routes/poisRouter.js
@@ -3,16 +3,11 @@ const router = express.Router();
 const db = require('../db.js')
 const validator = require('../query_validator')
 
-router.get('/pois', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).send(db.getPoi());
 });
 
-router.post('/pois', validator.bodyValidationRules(), validator.validate, (req, res) => {
-    let poi = db.createPoi(req.body);
-    res.status(201).send(req.body);
-});
-
-router.get('/pois/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     let poi = db.getPoi(req.params.id);
     if (poi) {
         res.status(200).send(poi);
@@ -21,7 +16,12 @@ router.get('/pois/:id', (req, res) => {
     }
 });
 
-router.put('/pois/:id', validator.bodyValidationRules(), validator.validate, (req, res) => {
+router.post('/', validator.bodyValidationRules(), validator.validate, (req, res) => {
+    let poi = db.createPoi(req.body);
+    res.status(201).send(req.body);
+});
+
+router.put('/:id', validator.bodyValidationRules(), validator.validate, (req, res) => {
     let poiExists = db.getPoi(req.params.id);
     let poi = db.setPoi(req.params.id, req.body);
 
@@ -32,7 +32,7 @@ router.put('/pois/:id', validator.bodyValidationRules(), validator.validate, (re
     }
 });
 
-router.delete('/pois/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     let poi = db.deletePoi(req.params.id);
     if (poi) {
         res.status(204).send();
