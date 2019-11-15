3549967b2e01da059f8741e7db0d70f2032b2477
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2019-11-15 10:39:00 +0200

    Added functionality to filter the objects by radius.

--- routes/poisRouter.js
+++ routes/poisRouter.js
@@ -2,9 +2,45 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db.js')
 const validator = require('../query_validator')
+const haversine = require('haversine-js')
 
-router.get('/', (req, res) => {
-    res.status(200).send(db.getPoi());
+router.get('/', (req, res, next) => {
+    if (!req.query) {
+        res.status(200).send(db.getPoi());
+    } else {
+        if (req.query["filter"] === "radius") {
+            next();
+        }
+    }
+
+}, validator.queryValidationRules(), validator.validate, (req, res) => {
+    let pois = db.getPoi();
+    let radius = req.query["radius"];
+    let array = [];
+
+    for (let index = 0; index < pois.length; index++) {
+        const element = pois[index];
+        const first = {
+            latitude: element.coordinates.lat,
+            longitude: element.coordinates.lng
+        }
+
+        const second = {
+            latitude: req.query["lat"],
+            longitude: req.query["lng"]
+        }
+
+        const options = {
+            radius: haversine.EARTH.KM
+        };
+
+        let result = haversine(first, second, options).toFixed(0);
+
+        if (result <= radius) {
+            array.push(element);
+        }
+    }
+    res.status(200).send(array);
 });
 
 router.get('/:id', (req, res) => {
